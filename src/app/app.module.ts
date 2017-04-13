a492353ceb7de3e48b8fca3a2540f7006289163c
Ana Raquel Campesan <anazard@outlook.com>
2017-04-12 21:03:28 -0400

    added navbar, fixed title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Router, RouterModule } from '@angular/router';
+
+import { ROUTES } from './app.routes';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(ROUTES),
     BrowserModule,
     FormsModule,
     HttpModule
@@ -19,4 +25,5 @@ import { NavbarComponent } from './navbar/navbar.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+export const ROUTES: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    // children: [
+    //
+    // ]
+  }
+];
